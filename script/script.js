c3780c08c7115fe8409aa45a959f370b6e9d19ff
Sadek-Mohammed <61293537+Sadek-Mohammed@users.noreply.github.com>
2021-09-14 22:15:09 +0200

    Coding Ezzat's design

--- script/script.js
+++ script/script.js
@@ -1,16 +1,28 @@
+const swiper = new Swiper(".swiper", {
+  allowSlideNext: true,
+  allowSlidePrev: true,
+  allowTouchMove: true,
+  centeredSlides: true,
+  keyboard: true,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 let x = false;
-const clicker = document.getElementById("control");
+const clicker = document.getElementById("menu-control");
 const menu = document.getElementById("menu");
 clicker.addEventListener("click", function () {
   if (!x) {
-    clicker.classList.add("close");
-    clicker.innerText = "x";
     menu.classList.add("active");
     x = true;
   } else {
-    clicker.classList.remove("close");
     x = false;
-    clicker.innerText = "s";
     menu.classList.remove("active");
   }
 });
