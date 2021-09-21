3a29f51231aa327ddf6cf4197ee157bffc65d398
Sadek-Mohammed <61293537+Sadek-Mohammed@users.noreply.github.com>
2021-09-21 14:55:23 +0200

    News has been finished

---
+++ script/news.js
@@ -0,0 +1,12 @@
+let x = false;
+const clicker = document.getElementById("menu-control");
+const menu = document.getElementById("menu");
+clicker.addEventListener("click", function () {
+  if (!x) {
+    menu.classList.add("active");
+    x = true;
+  } else {
+    x = false;
+    menu.classList.remove("active");
+  }
+});
